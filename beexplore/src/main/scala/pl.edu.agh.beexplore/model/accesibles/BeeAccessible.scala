064dcad8c65f1545d8b920726a67cdacfe7cfdfd
Wojciech Żdżarski <wojtek.zdz@gmail.com>
2019-12-08 17:25:21 +0100

    [XIN-2] Updated BeeAccessible.

--- beexplore/src/main/scala/pl.edu.agh.beexplore/model/accesibles/BeeAccessible.scala
+++ beexplore/src/main/scala/pl.edu.agh.beexplore/model/accesibles/BeeAccessible.scala
@@ -1,9 +1,8 @@
 package pl.edu.agh.beexplore.model.accesibles
 
-import javax.management.relation.Role
 import pl.edu.agh.beexplore.config.BeexploreConfig
 import pl.edu.agh.beexplore.model.Bee
-import pl.edu.agh.beexplore.model.Bee.{BeeRole, Scout}
+import pl.edu.agh.beexplore.model.Bee.BeeRole
 import pl.edu.agh.xinuk.model.{EmptyCell, GridPart}
 
 trait BeeAccessible[+T <: GridPart] {
